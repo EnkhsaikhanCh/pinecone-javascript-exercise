02ba098e01fa84c7c2022ea3c69f78e7b208c9c7
Enkhsaikhan <ch.enhsaihan@gmail.com>
2023-12-05 20:33:51 +0800

    All JavaScript pages had changed to HTML pages.
    + Exercise 1, 2, 3, and 4

--- scripts/adult_or_not.js
+++
@@ -1,7 +0,0 @@
-// Exercise - 1
-const age = prompt("Ta nasaa oruulnuu: ");
-if (age >= 18) {
-  console.log("nasand hursen");
-} else {
-  console.log("nasand hureegui");
-}
--- scripts/even_or_odd.js
+++
@@ -1,7 +0,0 @@
-const number = prompt("Ta toogoo oruulnuu: ");
-
-if (number % 2 == 0) {
-  console.log("tegsh too");
-} else {
-  console.log("sondgoi too");
-}
